c7a782a88f2eb55b1e7c2c8731d23522918b6cc6
bozhen964 <bozhencomputer964@gmail.com>
2022-06-17 14:41:14 +0300

    getting network image and text center them and give the text some decoration with borderradius for the image

--- hello_kurdistan/lib/main.dart
+++ hello_kurdistan/lib/main.dart
@@ -2,23 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(helloKurdistan());
- 
 }
 
 class helloKurdistan extends StatelessWidget {
   const helloKurdistan({Key? key}) : super(key: key);
+
   @override
-  
   Widget build(BuildContext context) {
-    
-    return const MaterialApp( 
-      debugShowCheckedModeBanner : false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(0, 255, 255, 255),
-        body: Center(
-          child: Text('Bozhen Ahmed',
-          style: TextStyle(color: Colors.amber)
-          )
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: null,
+            ),
+            title: Text('Kurdistan'),
+            backgroundColor: Color.fromARGB(255, 206, 64, 3)),
+        body: Container(
+          
+          margin: EdgeInsets.symmetric(vertical: 200),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child:
+                    Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Flag_of_Kurdistan.svg/1200px-Flag_of_Kurdistan.svg.png'),
+
+              ),
+              Text(
+                'Hello Kurdistan',
+                style: TextStyle(
+                    fontFamily: 'Helvetica',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 24.0),
+              )
+            ],
+          ),
         ),
       ),
     );
