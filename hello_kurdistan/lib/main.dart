baf3c1607e66d9d625fe8c450993e44776364563
bozhen964 <bozhencomputer964@gmail.com>
2022-06-19 23:23:09 +0300

    adding 3 images and you can selecet between them

--- hello_kurdistan/lib/main.dart
+++ hello_kurdistan/lib/main.dart
@@ -4,44 +4,92 @@ void main() {
   runApp(helloKurdistan());
 }
 
-class helloKurdistan extends StatelessWidget {
+class helloKurdistan extends StatefulWidget {
   const helloKurdistan({Key? key}) : super(key: key);
 
+  @override
+  State<helloKurdistan> createState() => _helloKurdistanState();
+}
+
+class _helloKurdistanState extends State<helloKurdistan> {
+  String _image1 =
+      'https://i.pinimg.com/originals/f3/c5/6f/f3c56f040ec8c416469d618442a43572.png';
+  String _image2 =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3IhkK_u1LbheH04PB_MYKSIsi5_N8YOrmlcNjuNxQHP_bUXGTrak_ZAsodYJ8QhXh0yU&usqp=CAU';
+  String _image3 =
+      'https://images.squarespace-cdn.com/content/v1/52d311f2e4b0c7203219d1cd/1442527167832-Q8LHW4TYCAWKSVP5FB85/image-asset.jpeg';
+  String _setImage =
+      '';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Color.fromARGB(255, 67, 0, 125),
         appBar: AppBar(
             leading: IconButton(
               icon: Icon(Icons.menu),
               onPressed: null,
             ),
             title: Text('Kurdistan'),
-            backgroundColor: Color.fromARGB(255, 206, 64, 3)),
-        body: Container(
-          margin: EdgeInsets.symmetric(vertical: 200),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30.0),
-                child:
-                    Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Flag_of_Kurdistan.svg/1200px-Flag_of_Kurdistan.svg.png'),
-              ),
-              Text(
-                'Hello Kurdistan',
-                style: TextStyle(
-                    fontFamily: 'Helvetica',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 24.0),
-              ),
-              
-            ],
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          ),
+            backgroundColor: Color.fromARGB(255, 98, 0, 115)),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 250,
+                  child: Image.network('$_setImage'),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _setImage = _image1;
+                    });
+                  },
+                  child: SizedBox(
+                    width: 80,
+                    height: 100,
+                    child: Image.network('$_image1'),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _setImage = _image2;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: SizedBox(
+                      width: 95,
+                      height: 200,
+                      child: Image.network('$_image2'),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _setImage = _image3;
+                    });
+                  },
+                  child: SizedBox(
+                    width: 80,
+                    height: 100,
+                    child: Image.network('$_image3'),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
